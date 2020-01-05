ca5531a062d390c2d2fba1a07727295d9c2f5cb6
Some8uddy <Some8uddy@gmail.com>
2020-01-05 17:50:46 +0300

    Stage2 - Concurrency. Minor code formatting.

--- src/main/java/stage2/concurrency/using_semaphore/Parking.java
+++ src/main/java/stage2/concurrency/using_semaphore/Parking.java
@@ -23,41 +23,41 @@ public class Parking {
 
         @Override
         public void run() {
-                System.out.printf("%s arrived to parking lot. (%d parking spots are available.)%n",
-                    name, countOfParkingSpotsLeft);
-                try {
-                    if (SEMAPHORE.tryAcquire(maxWaitingTimeInQueueSeconds, TimeUnit.SECONDS)) {
-                        int parkingSpotNumber = -1;
-                        synchronized (parkingSpots) {
-                            for (int i = 0; i < COUNT_OF_PARKING_SPOTS; i++)
-                                if (parkingSpots[i]) {
-                                    parkingSpots[i] = false;
-                                    countOfParkingSpotsLeft--;
-                                    parkingSpotNumber = i;
-                                    System.out.printf("\t%s occupied parking spot %d. (%d spots are available).%n",
-                                        name, i + 1, countOfParkingSpotsLeft);
-                                    break;
-                                }
-                        }
-
-                        int parkingTimeSeconds = (int) (Math.random() * 10);
-                        TimeUnit.SECONDS.sleep(parkingTimeSeconds);
-
-                        synchronized (parkingSpots) {
-                            parkingSpots[parkingSpotNumber] = true;
-                            countOfParkingSpotsLeft++;
-                        }
-
-                        SEMAPHORE.release();
-                        System.out.printf("%s left parking. (%d spots are available).%n",
-                            name, countOfParkingSpotsLeft);
-                    } else {
-                        System.out.printf("%s has been waiting too long and was redirected to another parking.%n", name);
+            System.out.printf("%s arrived to parking lot. (%d parking spots are available.)%n",
+                name, countOfParkingSpotsLeft);
+            try {
+                if (SEMAPHORE.tryAcquire(maxWaitingTimeInQueueSeconds, TimeUnit.SECONDS)) {
+                    int parkingSpotNumber = -1;
+                    synchronized (parkingSpots) {
+                        for (int i = 0; i < COUNT_OF_PARKING_SPOTS; i++)
+                            if (parkingSpots[i]) {
+                                parkingSpots[i] = false;
+                                countOfParkingSpotsLeft--;
+                                parkingSpotNumber = i;
+                                System.out.printf("\t%s occupied parking spot %d. (%d spots are available).%n",
+                                    name, i + 1, countOfParkingSpotsLeft);
+                                break;
+                            }
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                    int parkingTimeSeconds = (int) (Math.random() * 10);
+                    TimeUnit.SECONDS.sleep(parkingTimeSeconds);
+
+                    synchronized (parkingSpots) {
+                        parkingSpots[parkingSpotNumber] = true;
+                        countOfParkingSpotsLeft++;
+                    }
+
+                    SEMAPHORE.release();
+                    System.out.printf("%s left parking. (%d spots are available).%n",
+                        name, countOfParkingSpotsLeft);
+                } else {
+                    System.out.printf("%s has been waiting too long and was redirected to another parking.%n", name);
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
